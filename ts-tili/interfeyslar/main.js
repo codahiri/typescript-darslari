d53507c816634ee87736028dd975b096d8a358df
ilosrim <mirsolikhdev@gmail.com>
2021-10-01 11:22:38 +0500

    Interfayslar

---
+++ ts-tili/interfeyslar/main.js
@@ -0,0 +1,8 @@
+// Interfayslar
+function drowShape(shapa) {
+    // ...
+    console.log(shape.x.toString());
+}
+var shape;
+shape = { x: 10, y: 12 };
+drowShape(shape);
---
+++ ts-tili/interfeyslar/main.ts
@@ -0,0 +1,16 @@
+// Interfayslar
+
+interface Shape {
+  x: number;
+  y: number;
+}
+
+function drowShape(shapa: Shape){
+  // ...
+  console.log(shape.x.toString());
+  
+}
+
+let shape: Shape;
+shape = {x: 10, y: 12};
+drowShape(shape)
